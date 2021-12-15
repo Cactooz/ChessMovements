ddf8f0b555e9cdfa53a64a2e34b01ac8d49d4d51
Cactooz <54435874+Cactooz@users.noreply.github.com>
2021-12-15 11:31:54 +0100

    Base Chessboard.java class

---
+++ src/Chessboard.java
@@ -0,0 +1,132 @@
+public class Chessboard {
+	public static class Field {
+		private char row;
+		private byte column;
+		private Chesspiece piece = null;
+		private boolean marked = false;
+		
+		public Field(char row, byte column) {}
+		
+		public void put(Chesspiece piece) {}
+		
+		public Chesspiece take() {}
+		
+		public void mark() {}
+		
+		public void unmark() {}
+		
+		public String toString() {
+			String s = (marked)? "xx" : "--";
+			return (piece == null)? s : piece.toString ();
+		}
+	}
+	
+	
+	public static final int numberOfRows = 8;
+	public static final int numberOfColumns = 8;
+	
+	public static final int firstRow = 'a';
+	public static final int firstColumn = 1;
+	
+	private Field[][] fields;
+	
+	public Chessboard() {
+		fields = new Field[numberOfRows][numberOfColumns];
+		char    row = 0;
+		
+		byte    column = 0;
+		for (int r = 0; r < numberOfRows; r++)
+		{
+			row = (char) (firstRow + r);
+			column = firstColumn;
+			for (int c = 0; c < numberOfColumns; c++)
+			{
+				fields[r][c] = new Field (row, column);
+				column++;
+			}
+		}
+	}
+	
+	public String toString() {}
+	
+	public boolean isValidField(char row, byte column) {}
+	
+	
+	public abstract class Chesspiece {
+		private char color;
+		// w - white, b - black
+		
+		private char name;
+		// K - King, Q - Queen, R - Rook, B - Bishop, N - Knight,
+		// P – Pawn
+		
+		protected char row = 0;
+		protected byte column = -1;
+		
+		protected Chesspiece(char color, char name) {}
+		
+		public String toString() {
+			return "" + color + name;
+		}
+		
+		public boolean isOnBoard() {
+			return Chessboard.this.isValidField (row, column);
+		}
+		
+		public void moveTo(char row, byte column) throws NotValidFieldException {
+			if (!Chessboard.this.isValidField (row, column))
+				throw new NotValidFieldException ("bad field: " + row + column );
+			
+			this.row = row;
+			this.column = column;
+			
+			int    r = row - firstRow;
+			int    c = column - firstColumn;
+			Chessboard.this.fields[r][c].put (this);
+		}
+		
+		public void moveOut() {}
+		
+		public abstract void markReachableFields();
+		
+		public abstract void unmarkReachableFields();
+	}
+	
+	
+	public class Pawn extends Chesspiece {
+		public Pawn (char color, char name) {
+			super (color, name);
+		}
+		
+		
+		public void markReachableFields() {
+			byte    col = (byte) (column + 1);
+			if (Chessboard.this.isValidField (row, col))
+			{
+				int    r = row - firstRow;
+				int    c = col - firstColumn;
+				Chessboard.this.fields[r][c].mark ();
+			}
+		}
+		
+		public void unmarkReachableFields() {
+			byte    col = (byte) (column + 1);
+			if (Chessboard.this.isValidField (row, col))
+			{
+				int    r = row - firstRow;
+				int    c = col - firstColumn;
+				Chessboard.this.fields[r][c].unmark ();
+			}
+		}
+	}
+	
+	public class Rook extends Chesspiece {}
+	
+	public class Knight extends Chesspiece {}
+	
+	public class Bishop extends Chesspiece {}
+	
+	public class Queen extends Chesspiece {}
+	
+	public class King extends Chesspiece {}
+}
