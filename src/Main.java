999b90a6de972090cf3cab15f1e7187ea2213082
Cactooz <54435874+Cactooz@users.noreply.github.com>
2021-12-17 08:04:12 +0100

    Array with chessboard pieces

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
 	public static void main(String[] args) {
-	
+		Chessboard    chessBoard = new Chessboard ();
+		System.out.println (chessBoard + "\n");
+		
+		Chessboard.Chesspiece[]  pieces = new Chessboard.Chesspiece[6];
+		pieces[0] = chessBoard.new Pawn ('w');
+		pieces[1] = chessBoard.new Rook ('b');
+		pieces[2] = chessBoard.new Queen ('w');
+		pieces[3] = chessBoard.new Bishop ('w');
+		pieces[4] = chessBoard.new King ('b');
+		pieces[5] = chessBoard.new Knight ('w');
 	}
 }
