4e5ea6e216ce75edb4f069f6d58e0ac0ad005324
Cactooz <54435874+Cactooz@users.noreply.github.com>
2021-12-17 08:19:59 +0100

    Added loop for showing all pieces at a random location

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import java.util.Random;
+
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NotValidFieldException, InterruptedException {
 		Chessboard    chessBoard = new Chessboard ();
 		System.out.println (chessBoard + "\n");
 		
@@ -10,5 +12,24 @@ public class Main {
 		pieces[3] = chessBoard.new Bishop ('w');
 		pieces[4] = chessBoard.new King ('b');
 		pieces[5] = chessBoard.new Knight ('w');
+		
+		Random random = new Random();
+		for(Chessboard.Chesspiece chesspiece: pieces) {
+			char x = (char) (random.nextInt(8) + 'a');
+			byte y = (byte) (random.nextInt(8) + 1);
+			
+			//Move the piece onto the board and mark the possible movements
+			chesspiece.moveTo(x, y);
+			chesspiece.markReachableFields();
+			System.out.println (chessBoard + "\n");
+			
+			//Sleep for 1 second
+			Thread.sleep(1000);
+			
+			//Remove the piece and unmark the movements
+			chesspiece.unmarkReachableFields();
+			chesspiece.moveOut();
+			
+		}
 	}
 }
